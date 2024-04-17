541e11f76b945f3db4da59f8e50e749351462d38
Patrick Skipworth <pskipworth@fastmail.com>
2024-04-17 11:53:32 +0100

    GetUserInfo function complete, currently with hardcoded token info

--- MobileAcebook/Models/User.swift
+++ MobileAcebook/Models/User.swift
@@ -5,56 +5,60 @@
 //  Created by Josué Estévez Fernández on 01/10/2023.
 //
 
+import UIKit
+import Foundation
 import SwiftUI
 
-public struct User:Codable {
-    let username: String
-    let password: String
+class User: Decodable {
+    var _id: String
+    var email: String
+//    var password: String
+    var username: String
+    init(_id: String, email: String, password: String, username: String) {
+        self._id = _id
+        self.email = email
+//        self.password = password
+        self.username = username
+    }
+    func constructedUser() -> String {
+        return "User(\(self._id), \"\(self.email)\",  \"\(self.username)\")"
+//        If adding password info, add \"\(self.password)\",
+    }
+    
 }
 
-//func getUserInfo(completion: @escaping (User) -> Void) {
-//    var request = URLRequest(url: URL(string: "http://localhost:3000/users")!)
-//    request.httpMethod = "GET"
-//    request.setValue("user_id", forHTTPHeaderField: "Authorization")
-//    
-//    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-//        if let data = data {
-//            do {
-//                let userDetails = try JSONDecoder().decode(User.self, from: data)
-//                print(userDetails)
-//                completion(userDetails)
-//            } catch {
-//                print("error")
-//            }
-//        }
-//    }
-//}
-
+    public struct GetUserResponse: Decodable {
+        let userData: [User]
+        let token: String
+    }
+    
 func getUserInfo(completion: @escaping (User) -> Void) {
     var request = URLRequest(url: URL(string: "http://localhost:3000/users")!)
-    request.httpMethod = "GET"
-    
     // Set the Header values, which is the token to authenticate the session
-    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTMzNDM4OTgsImV4cCI6MTcxMzM0NDQ5OH0.hN51oZLhW6fXD8aRdGjBGS8xG337BfI-imbQtMqgl9A" // Replace this with your actual token you received back from logging in / getting all posts and stored
+    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjYxZTQ0YWE5ZGRjNmQ4ZTVjZDZiYmU5IiwiaWF0IjoxNzEzMzQ5NTE3LCJleHAiOjE3MTMzNTAxMTd9.qBV4FeLspNT2rXu8lGO0TPdQWCTfxfmnb2t6fKdZ-fI" // Replace this with your actual token you received back from logging in / getting all posts and stored
+    request.httpMethod = "GET"
     request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
     
     let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-        if let data = data {
-            do {
-                let userDetails = try JSONDecoder().decode(User.self, from: data)
-                // If it decodes successfully, print it out so you can see it in the console
-                print(userDetails)
-                completion(userDetails)
-            } catch {
-                let responseString = String(data: data, encoding: .utf8) ?? "Could not decode data"
-                   // If something goes wrong, print out the response string so we can see what the data looks like. We have to translate to a String to be able to read it (and not just see xx bytes)
-                print("Received data: \(responseString)")
-                // This error will give us more information
-                print("Error decoding data: \(error)")
-            }
-        } else if let error = error {
-            print("Network error: \(error)")
+        if let error = error {
+            print("Error: \(error)")
+            return
         }
+        guard let data = data else {
+            print("No data received")
+            return
+        }
+        print("Received data:", String(data: data, encoding: .utf8) ?? "")
+        let decoder = JSONDecoder()
+        do {
+            let response = try decoder.decode(GetUserResponse.self, from: data)
+            // If it decodes successfully, print it out so you can see it in the console
+            if let firstUser = response.userData.first {
+                completion(firstUser)
+            }
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
     }
     task.resume()
 }
