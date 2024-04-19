1e5e3a121e13b5a26a0dbb69b013ad33283920e5
Rachel Turrell <rachelturrell@gmail.com>
2024-04-19 14:12:13 +0100

    add image to sign up

--- MobileAcebook/Models/Posts.swift
+++ MobileAcebook/Models/Posts.swift
@@ -20,7 +20,7 @@ class PostStore: ObservableObject {
     
     func getPosts(completion: @escaping ([Post], Error?) -> Void) {
         var request = URLRequest(url: URL(string: "http://localhost:3000/posts")!)
-        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjYxZmQ5ODBiZWMwNjFmNjg3ZTM5ZDVlIiwiaWF0IjoxNzEzNDUzODczLCJleHAiOjE3MTM0ODk4NzN9.s9TBSSA3uu6D3g263IZnH170wyslOl-r8hbxznovPi4"
+        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjYyMjRlMTIwYzBlNjNhYmRhMTUxNzNlIiwiaWF0IjoxNzEzNTI0Mjg4LCJleHAiOjE3MTM1NjAyODh9.AHPliMxJMeavoJa8_uWycgY-LNhLGh0-VC-Y2-vIrLA"
         request.httpMethod = "GET"
         request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         
@@ -59,7 +59,7 @@ class PostStore: ObservableObject {
     
     func createPost(message: String, completion: @escaping ([Post]) -> Void) {
         var request = URLRequest(url: URL(string: "http://localhost:3000/posts")!)
-        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjYxZmQ5ODBiZWMwNjFmNjg3ZTM5ZDVlIiwiaWF0IjoxNzEzNDUzODczLCJleHAiOjE3MTM0ODk4NzN9.s9TBSSA3uu6D3g263IZnH170wyslOl-r8hbxznovPi4"
+        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjYyMjRlMTIwYzBlNjNhYmRhMTUxNzNlIiwiaWF0IjoxNzEzNTI0Mjg4LCJleHAiOjE3MTM1NjAyODh9.AHPliMxJMeavoJa8_uWycgY-LNhLGh0-VC-Y2-vIrLA"
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         let parameters: [String:Any] = ["message":message]
@@ -97,7 +97,7 @@ class PostStore: ObservableObject {
     
     func likePost(postId: String, completion: @escaping ([Post]) -> Void) {
         var request = URLRequest(url: URL(string: "http://localhost:3000/posts")!)
-        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjYxZmQ5ODBiZWMwNjFmNjg3ZTM5ZDVlIiwiaWF0IjoxNzEzNDUzODczLCJleHAiOjE3MTM0ODk4NzN9.s9TBSSA3uu6D3g263IZnH170wyslOl-r8hbxznovPi4"
+        let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjYyMjRlMTIwYzBlNjNhYmRhMTUxNzNlIiwiaWF0IjoxNzEzNTI0Mjg4LCJleHAiOjE3MTM1NjAyODh9.AHPliMxJMeavoJa8_uWycgY-LNhLGh0-VC-Y2-vIrLA"
         request.httpMethod = "PUT"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         let parameters: [String:Any] = ["postId":postId]
--- MobileAcebook/Models/User.swift
+++ MobileAcebook/Models/User.swift
@@ -20,12 +20,14 @@ public struct User: Codable {
     var email: String
     var password: String
     var username: String
+    var imgUrl: String?
     
-    init(_id: String, email: String, password: String, username: String) {
+    init(_id: String, email: String, password: String, username: String, imgUrl: String) {
         self._id = _id
         self.email = email
         self.password = password
         self.username = username
+        self.imgUrl = imgUrl
     }
     func constructedUser() -> String {
             return "User(\(self._id), \"\(self.email)\", \"\(self.password)\", \"\(self.username)\")"
